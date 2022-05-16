172e971d2ca042f474cedd89e396b2d44d2aa434
Mouez98 <mouez.matoui@gmail.com>
2022-05-16 23:46:11 +0100

    animated letters

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,36 @@
+import { useState } from 'react'
 import './Home.scss'
 
 import logoS from '../../assets/images/logo-s.png'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../animatedLetters'
 
 const Home = () => {
-     return (
-         <div className='container home-page'>
-            <div className='text-zone'>
-                <h1>Hi, <br /> I'm 
-                <img src={logoS} alt='developer' />
-                lobodan
-                <br />
-                web developer 
-            
-                </h1>
-                <h2>
-                    Frontent developer / ReactJs
-                </h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME </Link>
-            </div>
-         </div>
-     )
+  const [letterClass, setLetterClass] = useState('animated-letter');
+  const strArray = ['l','o','b','o','d','a','n']
+  const jobArray = ['w','e','b','d','e','v','e','l','o','p','e','r','.']
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _11`}>i,</span>
+          <br /> 
+          <span className={`${letterClass} _12`}>I</span>
+          <span className={`${letterClass} _13`}>'</span>
+          <span className={`${letterClass} _14`}>m</span>
+          <img src={logoS} alt="developer" />
+          <AnimatedLetters letterClass={letterClass} strArray={strArray} idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+        </h1>
+        <h2>Frontent developer / ReactJs</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default Home ;
+export default Home
